b96a574dd3ceb51c3decc8d655f2b2bdaaa52750
airRnot1106 <yngw.rintaro@gmail.com>
2023-01-14 14:07:28 +0900

    add server config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  nitro: {
+    preset: 'node',
+  },
+  devServer: {
+    host: '0.0.0.0',
+  },
+});
