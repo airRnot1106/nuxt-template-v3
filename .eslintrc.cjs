f0f13c957351dd8177ca722c674b5d0ca9e2eb7c
airRnot1106 <yngw.rintaro@gmail.com>
2023-01-14 14:00:59 +0900

    init eslint + prettier
    command: yarn add -D typescript eslint prettier eslint-config-prettier eslint-plugin-import eslint-plugin-vue @typescript-eslint/eslint-plugin @typescript-eslint/parser @nuxtjs/eslint-config-typescript

---
+++ .eslintrc.cjs
@@ -0,0 +1,85 @@
+module.exports = {
+  root: true,
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    '@nuxtjs/eslint-config-typescript',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    /* typescript */
+    'dot-notation': 'off',
+    'no-restricted-imports': [
+      'error',
+      {
+        patterns: [
+          '../*',
+          '~/*',
+          '~~/*',
+          './assets/*',
+          './components/*',
+          './pages/*',
+          './plugins/*',
+          './router/*',
+          './composables/*',
+          './server/*',
+          './store/*',
+          './types/*',
+          './utils/*',
+          './libs/*',
+          './*.vue',
+        ],
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        'groups': [
+          'builtin',
+          'external',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+          'type',
+        ],
+        'pathGroups': [
+          {
+            pattern: '{vue,vue-router,vite,@vitejs/plugin-vue}',
+            group: 'builtin',
+            position: 'before',
+          },
+          {
+            pattern: '@src/**',
+            group: 'parent',
+            position: 'before',
+          },
+        ],
+        'pathGroupsExcludedImportTypes': ['builtin'],
+        'alphabetize': {
+          order: 'asc',
+        },
+        'newlines-between': 'always',
+      },
+    ],
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      { prefer: 'type-imports' },
+    ],
+
+    /* nuxt */
+    'vue/multi-word-component-names': 'off',
+    'vue/require-v-for-key': 'off',
+  },
+};
