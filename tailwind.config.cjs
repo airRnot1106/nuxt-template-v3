13a29eb01e7e418d901f28c417a9868fe6a4559a
airRnot1106 <yngw.rintaro@gmail.com>
2023-01-14 16:45:38 +0900

    support tailwindcss in storybook
    command: yarn add -D tailwindcss @tailwindcss/postcss7-compat

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.html', './src/**/*.vue', './src/**/*.jsx'],
+  content: ['./src/**/*.{html,js,jsx,ts,tsx,vue}'],
   theme: {
     extend: {
       colors: {
